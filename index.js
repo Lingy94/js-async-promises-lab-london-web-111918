f91397e5210c25758b22f63b345126ae661c1184
Lingy94 <j.lingyx@gmail.com>
2019-01-09 17:08:37 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,43 @@ const questions = [
 ]
 
 let question;
+
+function appendQuestion(question){
+  return container = document.querySelector('.question-container').innerHTML = question['questionText']
+};
+
+function askQuestionThen(time){
+  question = questions[0];
+  appendQuestion(question);
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+      resolve(question);
+    },
+    time)
+  })
+};
+
+function removeQuestion(){
+  let container = document.querySelector('.question-container').innerHTML = ''
+};
+
+function askQuestionThenRemoveQuestion(time){
+  return askQuestionThen(time).then(removeQuestion)
+};
+
+function trueAndFalseButtons() {
+  return button = document.querySelectorAll('.btn.center-align')
+  // return button = document.querySelector('.true-false-list').querySelectorAll('.btn')
+};
+
+function toggleTrueAndFalseButtons() {
+   trueAndFalseButtons().forEach(button => {
+     button.classList.toggle('hide')
+  })
+};
+
+function displayQuestionOnClick() {
+  const displayQuestion = document.querySelector('.waves-effect.waves-light.btn')
+  return displayQuestion.addEventListener('click', function(){ toggleTrueAndFalseButtons(); askQuestionThenRemoveQuestion(5000)}
+  )
+};
